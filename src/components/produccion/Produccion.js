a1976c5565dcb5b48c700f233c252d2d7b74d789
joactr <j.m.molol@hotmail.com>
2022-02-16 09:13:18 +0100

    borrado produccion

--- src/components/produccion/Produccion.js
+++ src/components/produccion/Produccion.js
@@ -35,58 +35,6 @@ const Produccion = () => {
           <SideNav/>
           <div className="wrapperParamsProd">
 
-            <div className="prod">
-                <label className="textoProd">Número de perfiles (#)</label>
-                <input type="text" className="inputProd" id="perfiles" onKeyDown={onEnterPress}/>
-            </div>
-            <div className="prod">
-                <label className="textoProd">Hora Inicio Turno (dd/mm/aaa hh:mm)</label>
-                <input type="text" className="inputProd" id="inicio_turno" onKeyDown={onEnterPress}/>
-            </div>
-            <div className="prod">
-                <label className="textoProd">Hora Final turno (dd/mm/aaa hh:mm)</label>
-                <input type="text" className="inputProd" id="final_turno" onKeyDown={onEnterPress}/>
-            </div>
-            <div className="prod">
-                <label className="textoProd">Artículo (#)</label>
-                <input type="text" className="inputProd" id="articulo" onKeyDown={onEnterPress}/>
-            </div>
-            <div className="prod">
-                <label className="textoProd">Orden de Fabr. (#)</label>
-                <input type="text" className="inputProd" id="orden" onKeyDown={onEnterPress}/>
-            </div>
-            <div className="prod">
-                <label className="textoProd">Tiempo de ciclo (s.)</label>
-                <input type="text" className="inputProd" id="tiempo_ciclo" onKeyDown={onEnterPress}/>
-            </div>
-            <div className="prod">
-                <label className="textoProd">Ratio de Merma (%)</label>
-                <input type="text" className="inputProd" id="ratio_merma" onKeyDown={onEnterPress}/>
-            </div>
-            <div className="prod">
-                <label className="textoProd">Tiempo de Microparo (s.)</label>
-                <input type="text" className="inputProd" id="tiempo_microparo" onKeyDown={onEnterPress}/>
-            </div>
-            <div className="prod">
-                <label className="textoProd">Tiempo de avería (s.)</label>
-                <input type="text" className="inputProd" id="tiempo_averia" onKeyDown={onEnterPress}/>
-            </div>
-            <div className="prod">
-                <label className="textoProd">Tiempo de Falta Material (s.)</label>
-                <input type="text" className="inputProd" id="tiempo_falta_material" onKeyDown={onEnterPress}/>
-            </div>
-            <div className="prod">
-                <label className="textoProd">Tiempo de Paro 1 (s.)</label>
-                <input type="text" className="inputProd" id="tiempo_paro" onKeyDown={onEnterPress}/>
-            </div>
-            <div className="prod">
-                <label className="textoProd">Pausa producción</label>
-                <select className="selectReportar" className="selectProd" onChange={onChange} defaultValue={''} >
-                  <option selected disabled hidden></option>
-                  <option value="SI">SI</option>
-                  <option value="NO">NO</option>
-                </select>
-            </div>
 
 
 
