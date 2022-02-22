282690caa4d40ea1932cf183aa24604d3c91624d
joactr <j.m.molol@hotmail.com>
2022-02-22 10:32:04 +0100

    quitado copyright y arreglado css reporte

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -23,7 +23,7 @@ function Login(){
     <img src={background} className="backgroundLogin" alt="backgroundLogin"/>
     <div className="divisor"><img src={logo} className="logoLogin" alt="logo"/>
     <label className="descripcion">Visualiza, Controla  y Automatiza cualquier Proceso de tu Fábrica</label></div>
-    <footer className="footerLogin">Copyright © 2021 Enira Engineering</footer>
+    <footer className="footerLogin"></footer>
     <footer className="footerBlanco">
       <div className="divText"><label className="footerBlancoText" >Proyecto innovador</label></div>
       <img src={logoIVACE} className="logoIVACE" alt="logoIVACE" resizeMode="contain"/>
