b9055cc570f9ad524fcaa26ffec89102da38e876
joactr <j.m.molol@hotmail.com>
2022-03-31 08:26:25 +0200

    reporte cambiado

--- src/components/produccion/monitorizacion/Visualizar.js
+++ src/components/produccion/monitorizacion/Visualizar.js
@@ -1,39 +1,54 @@
 import React, {Fragment, useState, useEffect} from 'react';
 import SideNav from '../../sideNav/SideNav';
 import NavBarProd from '../NavBarProd';
-import NavBarMonitorizacionProd from './NavBarMonitorizacionProd'
-import NavBarSubprocesosMonit from './NavBarSubprocesosMonit'
 import './visualizar.css'
 
 export default function Visualizar() {
-  const[lineaState, setLineaState]=useState('Linea8');
-  const[subprocesoState, setSubprocesoState]=useState('Lateral');
+  const[lineaState, setLineaState]=useState('8');
+  const[subprocesoState, setSubprocesoState]=useState('laterales');
   const[reporteVisible,setReporteVisible]=useState(true);
 
-  const src = "https://app.powerbi.com/view?r=eyJrIjoiMWEyNDQ3NDktMDcyNy00MTBlLThmMGMtZGQyOGE3YmQ2ZDFmIiwidCI6IjlhMjNlNzRiLWVhMDktNDdjZi1hNmViLTQ2ZGZhY2Q2MzJmNSIsImMiOjl9&pageName=ReportSection"
-
-  var links = {"Linea8":[
-    {"firstName":"John", "lastName":"Doe"},
-    {"firstName":"Anna", "lastName":"Smith"},
-    {"firstName":"Peter", "lastName":"Jones"}
-    ]}
+  const src = "https://app.powerbi.com/view?r=eyJrIjoiZWRhOWExM2UtMjY1OS00YmVmLWJlMDQtYzc0OGNmMTkxNWEzIiwidCI6IjlhMjNlNzRiLWVhMDktNDdjZi1hNmViLTQ2ZGZhY2Q2MzJmNSIsImMiOjl9"
 
   useEffect(() => {
-      if(lineaState!='Linea8' || subprocesoState!='Lateral'){setReporteVisible(false);}  //De momento solo está la linea8 y lateral
+      console.log(lineaState)
+      console.log(subprocesoState)
+      if(lineaState!='8' || subprocesoState!='laterales'){setReporteVisible(false);}  //De momento solo está la linea8 y lateral
       else{setReporteVisible(true);}
       document.getElementById('reporteMonitTR').src = src;
     },[lineaState, subprocesoState]);
 
-
-
-
-
 return (
 <Fragment>
     <NavBarProd/>
-    <NavBarSubprocesosMonit stateChanger={setSubprocesoState}/>
+
     <SideNav/>
-    <NavBarMonitorizacionProd stateChanger={setLineaState}/>
+
+    <div className="wrapperLineaMonit">
+      <label>Línea:</label>
+      <select className="selectLineaMonit" onChange={(e)=>{setLineaState(e.target.value)}}>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option selected value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </select>
+      <label>Subproceso:</label>
+      <select className="selectLineaMonit" onChange={(e)=>{setSubprocesoState(e.target.value)}}>
+        <option value="cuadradillo">Cuadradillo</option>
+        <option value="testeros">Testeros</option>
+        <option selected value="laterales">Laterales</option>
+        <option value="fondo">Fondo</option>
+        <option value="envase">Envase</option>
+        <option value="piloto">Piloto</option>
+        <option value="total">Total</option>
+      </select>
+    </div>
     <iframe className={reporteVisible ? "reporteProd" : 'hidden'} id="reporteMonitTR" title="reportePowerBI" src={src} frameborder="0" allowFullScreen={true}></iframe>
 
 </Fragment>
